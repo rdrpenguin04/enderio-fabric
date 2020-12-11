1c703196b456714f91e6a42383de5ca0d5c82c30
Ray Redondo <rdrpenguin04@gmail.com>
2020-12-11 06:24:41 -0600

    Hello again video game. I see you have not changed.

--- src/main/java/com/lightning/enderio/misc/InfinitySpawning.java
+++ src/main/java/com/lightning/enderio/misc/InfinitySpawning.java
@@ -13,7 +13,7 @@ public class InfinitySpawning {
     public static class BedrockLightListener implements BlockUpdateCallback {
         @Override
         public ActionResult onBlockUpdate(World world, BlockPos pos, Block block, BlockPos posFrom) {
-        	final boolean isFire = block instanceof FireBlock;
+        	final boolean isFire = world.getBlockState(posFrom).getBlock() instanceof FireBlock;
         	final int posIdx = posFrom.hashCode() + world.getDimension().hashCode()*31;
         	final long worldTime = world.getTime();
         	if(isFire && world.getBlockState(pos).getBlock().is(Blocks.BEDROCK)) {
